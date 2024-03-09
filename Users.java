72dd2b50228acefd83331aaaa43d7175f0de76bd
unknown <andyduranmartinez@gmail.com>
2024-03-09 10:11:45 -0600

    modificacion

--- Users.java
+++ Users.java
@@ -1,2 +1,8 @@
+import java.util.ArrayList;
+
 public class Users {
+    private long id;
+    private String name;
+    private String age;
+    private ArrayList<Books> listRentedBooks = new ArrayList<>();
 }
