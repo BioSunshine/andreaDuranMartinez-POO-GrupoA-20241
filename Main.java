ba7a044b6c6436aa6f8d9d9315681131c94b81d4
unknown <andyduranmartinez@gmail.com>
2024-03-09 03:37:54 -0600

    cambios tarea 8

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+    /* Se desea crear un sistema para una librería
+    3 clases. Libreria debe contener arraylist libros y arraylist usuarios
+    El sistema debe tener una opción para registrar usuarios
+    El sistema debe tener una opción para registrar libros
+    El sistema debe tener una opción para realizar renta de libro
+    Usuario puede rentar uno o muchos libros pero un libro puede ser rentado solo por un usuario
+    Opción para listar todos usuarios registrados
+    Opcion listar todos libros registrados
+    opcion listar todos usuarios que hayan comprado al menos un libro
+    opcion listar libros no rentados
+    opcion listar libros rentados
+    menu usuario pueda interactuar con el y seleccionar las opciones deseadas
+     */
+    }
+}
